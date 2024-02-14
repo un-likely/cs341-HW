9ccc94f65f7baa4fd663cdbfc2aec37c17d7ecea
Leonie Freytag <freytag26@up.edu>
2024-02-14 03:51:45 -0800

    commit fixed files

--- hw3/public/javascripts/script.js
+++ hw3/public/javascripts/script.js
@@ -1,24 +1,26 @@
-//Leonie Freytag
-//JS File for cheesecake order form website
 
+//java script linked to index.html file 
+	
+        $(document).ready(function(){
+            $("form").submit(function(event){
+                event.preventDefault(); // Prevent the default form submission
 
-$(document).ready(function(){
-    $("form").submit(function(event){
-        event.preventDefault(); // Prevent the default form submission
+                // Check if the Notes contain the word "vegan"
+                if ($("#instructions").val().toLowerCase().includes("vegan")) {
+                    alert("Warning: Cheesecakes contain dairy!");
+                } else {
+                    // Replace the form section with a thank you message and order details
+                    var orderDetails = "Thank you! Your order has been placed\n" +
+                                       "Quantity: " + $("#quantity").val() + "\n" +
+                                       "Topping: " + $("input[name='topping']:checked").val() + "\n" +
+                                       "Notes: " + $("#instructions").val();
+                    $("form").replaceWith("<p>" + orderDetails + "</p>");
+                }
+            });
+	    
+	  /*  $("#months").click(function(event) {
+		$(.dropbtn).text() = $(this).text()";
+	    }); */
+		
+        });
 
-        // Check if the Notes contain the word "vegan"
-        if ($("#instructions").val().toLowerCase().includes("vegan")) 
-	{
-            alert("Warning: Cheesecakes contain dairy!");
-        } 
-	else 
-	{
-            // Replace the form section with a thank you message and order details
-            var orderDetails = "Thank you! Your order has been placed\n" +
-                               "Quantity: " + $("#quantity").val() + "\n" +
-                               "Topping: " + $("input[name='topping']:checked").val() + "\n" +
-                               "Notes: " + $("#instructions").val();
-            $("form").replaceWith("<p>" + orderDetails + "</p>");
-        }
-    });
-});
